757fb4bebc947af6fe5c0a449641c741787b2030
Andrew-Xu <qiyuanxu95@gmail.com>
2026-03-26 09:09:22 +0100

    updates

--- evaluation/dirs.py
+++ evaluation/dirs.py
@@ -9,10 +9,14 @@ machine = detect_machine()
 # if win, windows test in machine then auto switch to windows path, otherwise use mac path
 if any(win in machine for win in ["win", "windows"]): # windows
     dirs = {
-        "Dataset_root_dir": "C:/Users/qiyuanxu/Documents/DataHub/processed/",
         "merged_db_path": "C:/Users/qiyuanxu/Desktop/clear_2025_dataset.db",
+        "merged_processed_db_path": "C:/Users/qiyuanxu/Desktop/clear_2025_processed_dataset.db",
         "raw_db_dir": "C:/Users/qiyuanxu/Documents/DataHub/datasets",
+        "processed_db_dir": "C:/Users/qiyuanxu/Documents/DataHub/processed",
         "final_merged_db_path": "C:/Users/qiyuanxu/Desktop/dataset_meta.db",
+        
+        "globel_save_to": "C:/Users/qiyuanxu/Desktop/",
+        
         "DMD_only": {
             "dataset_db_dir": "C:/Users/qiyuanxu/Documents/DataHub/processed/CLEAR25_DMD/db/dataset_meta.db",
             "dataset_extracted_dir": "C:/Users/qiyuanxu/Documents/DataHub/processed/CLEAR25_DMD/",
@@ -21,6 +25,7 @@ if any(win in machine for win in ["win", "windows"]): # windows
             "dataset_dir": "C:/Users/qiyuanxu/Desktop/CLEAR25_DMD/",
             "output_db_dir": "C:/Users/qiyuanxu/Desktop/CLEAR25_DMD/db/dataset_meta.db",
         },
+        
         "Chromox_only": {
             "dataset_db_dir": "C:/Users/qiyuanxu/Documents/DataHub/processed/CLEAR25_Chromox/db/dataset_meta.db",
             "dataset_extracted_dir": "C:/Users/qiyuanxu/Documents/DataHub/processed/CLEAR25_Chromox/",
@@ -29,7 +34,11 @@ if any(win in machine for win in ["win", "windows"]): # windows
             "dataset_dir": "C:/Users/qiyuanxu/Desktop/CLEAR25_Chromox/",
             "output_db_dir": "C:/Users/qiyuanxu/Desktop/CLEAR25_Chromox/db/dataset_meta.db",
         },
+        
         "Yag_only": {
+            "dataset_db_dir": "C:/Users/qiyuanxu/Documents/DataHub/processed/CLEAR25_Yag/db/dataset_meta.db",
+            "dataset_extracted_dir": "C:/Users/qiyuanxu/Documents/DataHub/processed/CLEAR25_Yag/",
+            
             "output_dataset_dir": "C:/Users/qiyuanxu/Desktop/CLEAR25_Yag/dataset/",
             "dataset_dir": "C:/Users/qiyuanxu/Desktop/CLEAR25_Yag/",
             "output_db_dir": "C:/Users/qiyuanxu/Desktop/CLEAR25_Yag/db/dataset_meta.db",
