84840ba6e0b106ca35e666d090a7cde5458c8597
Andrew-XQY <710716661@qq.com>
2026-03-28 19:39:13 +0100

    updates

--- evaluation/dirs.py
+++ evaluation/dirs.py
@@ -58,17 +58,48 @@ if any(win in machine for win in ["win", "windows"]): # windows
 elif any(mac in machine for mac in ["mac", "darwin"]): # Mac
     dirs = {
         "merged_db_path": "/Users/andrewxu/Desktop/clear_2025_dataset.db",
+        "merged_processed_db_path": "/Users/andrewxu/Desktop/clear_2025_processed_dataset.db",
         "raw_db_dir": "/Users/andrewxu/Documents/DataHub/datasets",
+        "processed_db_dir": "/Users/andrewxu/Documents/DataHub/processed",
         "final_merged_db_path": "/Users/andrewxu/Desktop/dataset_meta.db",
+        
+        "globel_save_to": "/Users/andrewxu/Desktop/",
+        
+        "DMD_lab": {
+            "dataset_db_dir": "/Users/andrewxu/Documents/DataHub/processed/2025-11-06/db/dataset_meta.db",
+            "dataset_extracted_dir": "/Users/andrewxu/Documents/DataHub/processed/2025-11-06/"
+        },
         "DMD_only": {
+            "dataset_db_dir": "/Users/andrewxu/Documents/DataHub/processed/CLEAR25_DMD/db/dataset_meta.db",
+            "dataset_extracted_dir": "/Users/andrewxu/Documents/DataHub/processed/CLEAR25_DMD/",
+            
             "output_dataset_dir": "/Users/andrewxu/Desktop/CLEAR25_DMD/dataset/",
             "dataset_dir": "/Users/andrewxu/Desktop/CLEAR25_DMD/",
             "output_db_dir": "/Users/andrewxu/Desktop/CLEAR25_DMD/db/dataset_meta.db",
         },
         "Chromox_only": {
+            "dataset_db_dir": "/Users/andrewxu/Documents/DataHub/processed/CLEAR25_Chromox/db/dataset_meta.db",
+            "dataset_extracted_dir": "/Users/andrewxu/Documents/DataHub/processed/CLEAR25_Chromox/",
+            
             "output_dataset_dir": "/Users/andrewxu/Desktop/CLEAR25_Chromox/dataset/",
             "dataset_dir": "/Users/andrewxu/Desktop/CLEAR25_Chromox/",
             "output_db_dir": "/Users/andrewxu/Desktop/CLEAR25_Chromox/db/dataset_meta.db",
+        },
+        "Yag_only": {
+            "dataset_db_dir": "/Users/andrewxu/Documents/DataHub/processed/CLEAR25_Yag/db/dataset_meta.db",
+            "dataset_extracted_dir": "/Users/andrewxu/Documents/DataHub/processed/CLEAR25_Yag/",
+            
+            "output_dataset_dir": "/Users/andrewxu/Desktop/CLEAR25_Yag/dataset/",
+            "dataset_dir": "/Users/andrewxu/Desktop/CLEAR25_Yag/",
+            "output_db_dir": "/Users/andrewxu/Desktop/CLEAR25_Yag/db/dataset_meta.db",
+        },
+        "Chromox_Laser_only": {
+            "output_dataset_dir": "/Users/andrewxu/Desktop/CLEAR25_Chromox_Laser/dataset/",
+            "output_db_dir": "/Users/andrewxu/Desktop/CLEAR25_Chromox_Laser/db/dataset_meta.db",
+        },
+        "YAG_Laser_only": {
+            "output_dataset_dir": "/Users/andrewxu/Desktop/CLEAR25_Yag_Laser/dataset/",
+            "output_db_dir": "/Users/andrewxu/Desktop/CLEAR25_Yag_Laser/db/dataset_meta.db",
         }
     }
 
